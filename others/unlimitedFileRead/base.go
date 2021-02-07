b5b623f199196578679accd52b0a5a8b4f1cf6a3
jibenliu <liuzb@mingyuanyun.com>
2021-02-07 18:19:28 +0800

    添加代码

--- others/unlimitedFileRead/base.go
+++ others/unlimitedFileRead/base.go
@@ -1,1 +1,19 @@
-package unlimitedFileRead
+package main
+
+import (
+	"fmt"
+	"github.com/edsrzf/mmap-go"
+	"os"
+)
+
+func main() {
+	f, _ := os.OpenFile("./file", os.O_RDWR, 0644)
+	defer f.Close()
+
+	maps, _ := mmap.Map(f, mmap.RDWR, 0)
+	defer maps.Unmap()
+	fmt.Println(string(maps))
+
+	maps[0] = 'x'
+	maps.Flush()
+}
--- others/unlimitedFileRead/go.mod
+++ others/unlimitedFileRead/go.mod
@@ -1,3 +1,8 @@
 module unlimitedFileRead
 
 go 1.15
+
+require (
+	github.com/edsrzf/mmap-go v1.0.0
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
